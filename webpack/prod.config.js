eb6ef26ef7acb313710f05eeb4e9b4127ea3dedb
bosiditan <1356075038@qq.com>
2020-03-17 21:12:52 +0800

    Update prod.config.js

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname,'../dist'),
-    libraryTarget: 'commonjs'
+    libraryTarget: 'commonjs2'
   },
   module: {
     rules: [
